b8d7bfe128a126d27568f30b7ac1bdc2506c0d5d
timmythetrtl <89177115+timmythetrtl@users.noreply.github.com>
2023-05-04 13:18:51 -0500

    Fix

--- Assets/Scripts/Dungeon/DungeonGenerator.cs
+++ Assets/Scripts/Dungeon/DungeonGenerator.cs
@@ -1,14 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 using UnityEngine;
 
 public class DungeonGenerator : MonoBehaviour
 {
     public DungeonGenerationData dungeonGenerationData;
-
+    public GameObject[] prefabs;
     private List<Vector2Int> dungeonRooms;
-
     private float room = 19.92f;
 
     private void Start()
@@ -19,8 +17,9 @@ public class DungeonGenerator : MonoBehaviour
 
     private void SpawnRooms(IEnumerable<Vector2Int> rooms)
     {
-        RoomController.instance.LoadRoom("Start", 0 ,0);
-        foreach(Vector2Int roomLocation in rooms)
+        RoomController.instance.LoadRoom("Start", 0, 0);
+
+        foreach (Vector2Int roomLocation in rooms)
         {
             if (roomLocation == dungeonRooms[dungeonRooms.Count - 1] /*&& (roomLocation == Vector2Int.zero)*/)
             {
@@ -30,20 +29,13 @@ public class DungeonGenerator : MonoBehaviour
             {
                 RoomController.instance.LoadRoom("Empty", roomLocation.x, roomLocation.y);
 
-                // Get a list of all prefab files in the directory
-                string prefabDirectory = @"D:\\Docs\\Unity\\Final\\Assets\\Resources\\Platforms";
-                string[] prefabFiles = Directory.GetFiles(prefabDirectory, "*.prefab");
-
-                // Choose a random prefab from the list
-                string randomPrefabName = Path.GetFileNameWithoutExtension(prefabFiles[Random.Range(0, prefabFiles.Length)]);
+                // Choose a random prefab from the array
+                GameObject randomPrefab = prefabs[Random.Range(0, prefabs.Length)];
 
                 // Spawn the chosen prefab at the same location as the "Empty" room
-                GameObject prefab = Resources.Load<GameObject>("Platforms/" + randomPrefabName);
-                Instantiate(prefab, new Vector3(-1.47f + -0.007124f, room), Quaternion.identity);
+                Instantiate(randomPrefab, new Vector3(-1.47f + -0.007124f, room), Quaternion.identity);
                 room = room + 18f;
-
             }
         }
-    } 
-
+    }
 }
